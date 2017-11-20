aacbdf5d7633d7f29af27ff32c6c9dd85b25759a
Davy Braillon <d.braillon@outlook.com>
2017-11-20 22:51:16 +0100

    Added LoriaNET for easier use in win environment

---
+++ src/LoriaNET/Program.cs
@@ -0,0 +1,32 @@
+﻿using Loria.Core;
+using System.Diagnostics;
+using System.Threading;
+
+namespace LoriaNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var engine = new Engine();
+            engine.LiveAsync();
+
+            Debug.WriteLine("Modules loaded:");
+            engine.ModuleFactory.Items.ForEach(m => Debug.WriteLine($"  {m.Name.PadRight(15, ' ')}\t{m.Description}"));
+
+            Debug.WriteLine("Activities loaded:");
+            engine.ActivityFactory.Items.ForEach(m => Debug.WriteLine($"  {m.Name.PadRight(15, ' ')}\t{m.Action}"));
+
+            Debug.WriteLine("Messengers loaded:");
+            engine.MessengerFactory.Items.ForEach(m => Debug.WriteLine($"  {m.Name.PadRight(15, ' ')}\t{m.Action}"));
+
+            Debug.WriteLine("Listeners loaded:");
+            engine.ListenerFactory.Items.ForEach(m => Debug.WriteLine($"  {m.Name}"));
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
